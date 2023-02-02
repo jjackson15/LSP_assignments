73b0a6613bd22d1ff4eea10e416f554e8dbdef94
jjackson15 <jacksonjennai81503@gmail.com>
2023-02-02 18:41:33 -0500

    created class

---
+++ large_scale_spring/src/org/howard/edu/lsp/assignment2/WordCounting.java
@@ -0,0 +1,34 @@
+package org.howard.edu.lsp.assignment2;
+
+public class WordCounting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+//def wordcounting(string):
+//	  new_string = ""
+//	  lower_string = string.lower()
+//	  for i in string:
+//	    if not i.isnumeric():
+//	      new_string += i
+//	  # return new_string
+//	#   print(new_string)
+//	  x = new_string.split()
+//	  # print(x)
+//	  
+//	  word_count = {}
+//	  for word in x:
+//	    if len(word) > 3 and word not in word_count:
+//	      word_count[word] = 1
+//	    elif len(word) > 3 and word in word_count:
+//	      word_count[word] += 1
+//	    else:
+//	      continue
+//
+//	  
+//
+//	  for i in word_count:
+//	    print(i, ':', word_count[i])
